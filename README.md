8069bb8e50a20423ac090b21c33950784650ac27
Restray <timothee.belhomme@gmail.com>
2017-05-13 20:27:39 +0200

    Change the gui system
    Change the GUI to a web server

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Un simple Scheduler avec un systeme de plugin et de log
 
 # Usage
 
-$ python scheduler run
+$ python scheduler.py run
 
 Pour faire tourner le scheduler
+
+# Usage of the GUI
+
+$ python gui.py
+
+Puis allez dans votre navigateur et rentrer comme URL : " localhost:5000 "
--- gui.py
+++ gui.py
@@ -1,43 +1,22 @@
-from Tkinter import *
-import time
+from flask import Flask
+from flask import render_template
 import os
 
-class SampleApp(Tk):
-    def __init__(self, *args, **kwargs):
-        self.change = "Stats"
+app = Flask(__name__)
 
-        Tk.__init__(self, *args, **kwargs)
-        self.geometry('{}x{}'.format(768, 400))
-        self.t = Text(self)
-        self.t.grid()
-        self.update()
+@app.route("/")
+def hello():
+    entries = {}
+    for task in open("%s\\libraries\\gui_interface.txt"%(os.path.dirname(os.path.abspath(__file__)))):
+        if ":" in task:
+            if "True" in task or "False" in task:
+                tasked = task.split(":")
+                if "True" in tasked[1]:
+                    task_stat = "Work"
+                else:
+                    task_stat = "Error"
+                entries[tasked[0]] = task_stat
+    return render_template('index.html', entries=entries)
 
-        self.bouton_quitter = Button(self, text="Refresh", command=self.update_clock)
-        self.bouton_quitter.pack(side="right")
-
-    def update_clock(self):
-
-        self.i = 0
-
-        self.t.destroy()
-
-        self.t = Text(self)
-        self.t.grid()
-
-        self.gui_interface = open("%s\\libraries\\gui_interface.txt"%(os.path.dirname(os.path.abspath(__file__))), "r")
-        self.num_lines = sum(1 for line in open("%s\\libraries\\gui_interface.txt"%(os.path.dirname(os.path.abspath(__file__)))))
-        for line in self.gui_interface:
-            if self.i == self.num_lines-1:
-                self.t.insert(0.0, "%s\n"%(line))
-                self.i += 1
-            else:
-                self.t.insert(0.0, "%s"%(line))
-                self.i+=1
-
-        self.t["state"] = DISABLED
-        self.gui_interface.close()
-        self.update()
-
-if __name__== "__main__":
-    app = SampleApp()
-    app.mainloop()
+if __name__ == "__main__":
+    app.run()
